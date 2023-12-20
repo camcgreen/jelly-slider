205a9240245d8d674f0f7496906604f0afd389f1
camcgreen <c.c.green@outlook.com>
2023-12-20 11:43:00 +0000

    Change images and text

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,9 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <header className={styles.header}>
+        <img src='/images/logo.svg' alt='Logo - Cam Green' />
+      </header>
       <main className={styles.container}>
         <Slider />
       </main>
